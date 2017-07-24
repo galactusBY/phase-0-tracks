fdd62c5c0899e8003f80cc8119be2136f6ef8699
“Taylor <taysvendsen@gmail.com>
2017-07-24 00:06:12 -0700

    created hiring program to detect potential vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+#What is your name?
+#How old are you? What year were you born?
+#Our company cafeteria serves garlic bread. Should we order some for you?
+#Would you like to enroll in the company’s health insurance?
+
+puts "What is your name"
+fullname = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Do you like Garlic?"
+likes_garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
+
