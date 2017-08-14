49fc1dedab9f86293cffef6e23baeeab3ae103bf
“Taylor <taysvendsen@gmail.com>
2017-08-14 00:37:11 -0700

    created pseudocode for the solo challenge

---
+++ js/algos.js
@@ -0,0 +1,20 @@
+// Release 0
+// split array in parts
+// add up the letters in each part of the array value
+// create a method that will compare those values and return that which has the longest amount of chars and gives it as the longest word/phrase
+
+// Release 1
+// Split all hash key values and put them into an array
+// split all hash values and put them into an array
+// compare each value in the key array with a true or false result
+// compare each value in the value array with a true or false result
+// print the comparison results as a true or false statement
+// possibly print the result to a string for the user to statement
+
+// Release 2
+// // Create a method that will take an integer and give back a random series of word that is the same length as that integer's value.
+// Max chars length of 10 and Min of 1
+// create method that takes integer as an argument
+// use a random letter generator to populate the chars in a word
+// send that completed word to an array
+// find a way to randomly generate a number between 1-10 and feed that into the method for creating a random word.
